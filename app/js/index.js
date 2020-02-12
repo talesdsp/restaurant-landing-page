1ab0a0f7ddc46852fe585a5e17ed0b96f0554a51
Tales Pereira <talesdsp@gmail.com>
2020-02-11 21:24:55 -0300

    add js based smooth scroll

---
+++ app/js/index.js
@@ -0,0 +1,72 @@
+const burger = document.querySelector(".burger");
+const welcomeMenu = document.querySelector(".welcome__menu");
+burger.addEventListener("click", toggleMenu);
+
+document.addEventListener("aos:in:my-filter", ({detail}) => {
+  console.log();
+  detail.style.filter = "blur(3px)";
+});
+
+function toggleMenu(e) {
+  welcomeMenu.classList.toggle("open");
+  const el = burger.children[0].classList;
+
+  el.toggle("open");
+  burger.setAttribute("aria-expanded", el.contains("open"));
+}
+
+const anchor = document.querySelectorAll('a[href^="#"]');
+
+function smooth(target, duration) {
+  let targetPosition = target.getBoundingClientRect().top;
+  let startPosition = window.pageYOffset;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+
+    let timeElapsed = currentTime - startTime;
+    let run = ease(timeElapsed, startPosition, targetPosition, duration);
+
+    window.scrollTo(0, run);
+
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+  window.requestAnimationFrame(animation);
+}
+
+anchor.forEach((v) => {
+  v.addEventListener("click", function(evt) {
+    let target = document.querySelector(this.hash);
+    console.log(target);
+    evt.preventDefault();
+    smooth(target, 800);
+    setTimeout(() => addFocus(target), 1000);
+  });
+});
+
+function addFocus(t) {
+  t.focus();
+  console.log(document.activeElement);
+  if (document.activeElement === t) {
+    console.log("added");
+    return false;
+  } else {
+    console.log("try again");
+    t.setAttribute("tabindex", "-1"); // Adding tabindex for elements not focusable
+    t.focus(); // Set focus again
+  }
+}
+
+window.AOS.init({
+  easing: "ease",
+  duration: 1800,
+  once: true
+});
