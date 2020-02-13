6e611af6cbd112d2642aa0f814f1f25084bdcb11
Tales Pereira <talesdsp@gmail.com>
2020-02-12 22:54:10 -0300

    replace css filter with pre blured img

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(function(registration) {
-      console.log("ServiceWorker registration successful with scope:", registration.scope);
-    })
-    .catch(function(error) {
-      console.log("ServiceWorker registration failed:", error);
-    });
-}
+// if (navigator.serviceWorker) {
+//   navigator.serviceWorker
+//     .register("/sw.js")
+//     .then(function(registration) {
+//       console.log("ServiceWorker registration successful with scope:", registration.scope);
+//     })
+//     .catch(function(error) {
+//       console.log("ServiceWorker registration failed:", error);
+//     });
+// }
